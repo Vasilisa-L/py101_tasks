dd69f42f1ce14b1e34ad56d765a51aa706e8e04f
Vasilisa-L <yulia.fomina.2806@ya.ru>
2020-11-04 11:21:36 +0300

    guess_num

--- guess_num.py
+++ guess_num.py
@@ -13,6 +13,29 @@
 
 Тебе может понадобится модуль random, цикл while и ветвления
 """
+from random import randint
+import sys
 
 if __name__ == '__main__':
-    pass
+    print('Hey, darling!\nAre u ready for a little game?\nI\'m thinking of a number from one to 1000000\nCan u guess it?')
+    grange = 100
+    number_to_guess = randint(0, grange)
+    user_number = 0
+    while user_number != number_to_guess:
+        user_number = input('Any guess? > ')
+        if user_number == '' or user_number == 'exit':
+            sys.exit()
+        try:
+            user_number = int(user_number)            
+        except ValueError:
+            print('Stop doing that! That\'s not a number! Try again!')
+            continue
+        if user_number < 0 or user_number > grange:
+            print('Not in range! Try again!')
+            continue
+        elif int(user_number) > number_to_guess:
+            print('Too much. My number is less.')
+        elif int(user_number) < number_to_guess:
+            print('You need something greater.')
+    print('You got it! Goog job!')
+            
