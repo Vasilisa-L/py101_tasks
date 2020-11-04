44deaacc57312e179778b55f039b103c519b8893
Vasilisa-L <yulia.fomina.2806@ya.ru>
2020-11-04 12:22:37 +0300

    password

--- password.py
+++ password.py
@@ -7,4 +7,22 @@
 """
 
 if __name__ == '__main__':
-    pass
+    user_password = input('Enter pass > ')
+    flag_length = len(user_password) >= 8
+    flag_letters = False
+    flag_numbers = False
+    for i in user_password:
+        if i.isdigit(): 
+            flag_numbers = True
+        if i.isalpha(): 
+            flag_letters = True 
+    if flag_length and flag_letters and flag_numbers:
+        print('Сложный')
+    else:
+        print('Простой:')
+        if not flag_length:
+            print('\tПароль должен быть минимум 8 символов')
+        if not flag_numbers:
+            print('\tПароль должен содержать минимум 1 цифру')
+        if not flag_letters:
+            print('\tПароль должен содержать минимум 1 букву')
